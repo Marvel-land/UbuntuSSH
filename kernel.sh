45ebcfed323d6f026e9a19462ede5ef4c1904103
MarvelMathesh <76835656+MarvelMathesh@users.noreply.github.com>
2021-11-16 13:00:09 +0530

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/MarvelMathesh/kernel-test-2 -b $BRANCH kernel --depth 1 && cd kernel
 
 sudo apt install ccache
-export USE_CCACHE=1 && export CCACHE_EXEC=/usr/bin/ccache && ccache -M 10G && export CCACHE_COMPRESS=1
+export USE_CCACHE=1 && export CCACHE_EXEC=/usr/bin/ccache && ccache -M 5G && export CCACHE_COMPRESS=1
 
 export TZ='Asia/Kolkata'
 
@@ -22,7 +22,7 @@ TIME=$(date "+%r")
 #tg_post_msg "<b>🔨 Marvel Kernel Build Triggered!</b>%0A<b>Builder Name: </b><code>${KBUILD_BUILD_USER}</code>%0A<b>Builder Host: </b><code>${KBUILD_BUILD_HOST}</code>%0A<b>Build For: </b><code>land</code>%0A<b>Build Date: </b><code>$DATE</code>%0A<b>Build Time: </b><code>$TIME</code>%0A<b>Build started on: </b><code>GitHub Actions</code>%0A<b>Compiler Info: </b><code>Proton Clang</code>%0A%0A1:00 ●━━━────────── 15:00 ⇆ㅤㅤㅤ ㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤㅤㅤ↻"
 
 ./build_kernel.sh g
-cd out && curl -sL https://git.io/file-transfer | sh && ./transfer wet *.zip
+cd out
 export kernel=$(ls *.zip)
 curl -F document=@$kernel "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
         -F chat_id=$TG_CHAT_ID\
